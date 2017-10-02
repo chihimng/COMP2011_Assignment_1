506510391ea04e320559a787e77cb5c119db512d
Elliot Ng <chihimng.io@gmail.com>
2017-10-02 22:49:08 +0800

    implement isGameOver

--- COMP2011_Assignment_1/main.cpp
+++ COMP2011_Assignment_1/main.cpp
@@ -172,7 +172,7 @@ int getGameState(char s1, char s2, char s3, char s4, char s5, char s6, char s7, 
  */
 bool isGameOver(char s1, char s2, char s3, char s4, char s5, char s6, char s7, char s8, char s9)
 {
-
+    return getGameState(s1, s2, s3, s4, s5, s6, s7, s8, s9) != 0;
 }
 
 /*
